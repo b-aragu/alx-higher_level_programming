b0a92b1e14529facabffc1810d76135dd84ae29e
baragu <baraguantonyy@gmail.com>
2023-03-14 02:54:43 +0300

    none due to lack of return value

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
-# 5-no_c.py
-
-
 def no_c(my_string):
-    """Remove all characters c and C from a string."""
-    copy = [x for x in my_string if x != 'c' and x != 'C']
-    return ("".join(copy))
+    result = ""
+    for z in my_string:
+        if z == 'C' or z == 'c':
+            continue
+        result += z
+    return result
+
