f219c1962c3b2bab9f2e2b73fc256430259bc296
baragu <baraguantonyy@gmail.com>
2023-05-10 01:02:46 +0300

    molly

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,4 @@
 # 5-print_comb2.py
 
 for i in range(100):
-    if i == 99:
-        print("{}".format(i))
-    else:
-        print("{:02d}".format(i), end=", ")
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
