f1bbc5dd8d9556aef076ee7cebd584eb167d2363
baragu <baraguantonyy@gmail.com>
2023-05-11 13:19:27 +0300

    magic

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,10 @@
-def my_function(a, b, c):
-    if a < b:
-        return c
-    elif c > b:
-        return a + b
-    else:
-        return a * b - c
+#!/usr/bin/python3
+# byte code 
 
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
