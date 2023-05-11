a76bcd081b5e03ef3932789c7f2b51f1954d9e7b
baragu <baraguantonyy@gmail.com>
2023-05-12 01:58:35 +0300

    int evaluate

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# who are you?
+
+
+if __name__ == "__main__":
+    import hidden_4
+
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# everything can be imported 5-variable_load.py
+
+
+if __name__ == "__main__":
+    import variable_load_5
+    print(variable_load_5.a)
