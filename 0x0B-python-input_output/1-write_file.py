f74012b33e6b00b6ec5bd35d5ded7fc03d134150
baragu <baraguantonyy@gmail.com>
2023-06-14 05:16:41 +0300

    a

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,14 @@
 
 
 def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
     with open(filename, encoding="utf-8") as f:
         return f.write("text")
 baragu@baragu254:~/Documents/ALx/
