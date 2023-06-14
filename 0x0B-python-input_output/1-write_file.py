2cefa07010ccb3438bcb8878a11bcd7a9edec1bb
baragu <baraguantonyy@gmail.com>
2023-06-14 05:11:11 +0300

    write

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-read_file.py
+
+"""text file reading function"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, encoding="utf-8") as f:
+        return f.write("text")
+baragu@baragu254:~/Documents/ALx/
