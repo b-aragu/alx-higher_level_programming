afdd403fab012fc9dc9886308f0c87fded8cc788
baragu <baraguantonyy@gmail.com>
2023-06-29 20:27:21 +0300

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py
+
+"""square with size"""
+
+
+class Square:
+    """rep a square"""
+    def __init__(self, size):
+        """initialize a new square
+
+        Args:
+            size: the size of the new square
+            """
+        self.__size = size
