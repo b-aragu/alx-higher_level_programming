f4fb14b06eb1091e70b7b2ae9eb9eee0fa31e336
baragu <baraguantonyy@gmail.com>
2023-06-30 21:38:55 +0300

    772 lovw

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# 1-square.py
+
+"""square with size"""
+
+
+class Square:
+    """rep a square"""
+    def __init__(self, size=0):
+        """initialize a new square
+
+        Args:
+            size: the size of the new square
+        exceptions:
+            TypeError: if size is not an integer(size must be an integer)
+            ValueError: if size is less than 0 (size must be >-= 0)
+            """
+        self.size = size
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """A getter """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """A setter attribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
