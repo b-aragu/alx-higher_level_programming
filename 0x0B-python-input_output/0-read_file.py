df0da01c171e4cf3d86e13a61584d4a587629027
Baragu <baraguantonyy@gmail.com>
2023-08-15 20:13:38 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 0-read_file.py
-
-"""text file reading function"""
-
-
-def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
