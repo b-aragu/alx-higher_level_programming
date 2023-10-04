5c4d90c25c9d6b81b8387716ac92c14f218f9bdf
baragu <baraguantonyy@gmail.com>
2023-10-04 18:15:44 +0300

    private

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,10 +27,9 @@ Instantiation: def __init__(self, width=0, height=0):
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("Width must be an integer")
-        elif (value < 0):
+        if (value < 0):
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -40,7 +39,6 @@ Instantiation: def __init__(self, width=0, height=0):
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif (value < 0):
+        if (value < 0):
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
