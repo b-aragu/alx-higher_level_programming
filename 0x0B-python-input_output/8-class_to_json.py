c787846e6b5a2101532ba6bf73347b1a02cc54f0
baragu <baraguantonyy@gmail.com>
2023-10-11 20:17:41 +0300

    hash

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""class to JSON"""
+import json
+
+
+def class_to_json(obj):
+    """
+returns dict desription with simple data structure for Json serialization
+args: 
+    obj - an instance of a class
+
+    """
+    return obj.__dict__
