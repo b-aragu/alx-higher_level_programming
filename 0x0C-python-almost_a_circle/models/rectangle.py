d45ca8a6fd590ede088a9595b1033813fa5956d4
baragu <baraguantonyy@gmail.com>
2023-10-16 22:03:29 +0300

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,8 +25,15 @@ class Rectangle(Base):
 
     def display(self):
         """print to stdout rectangle instance with #"""
-        for i in range(self.__height):
-            print('#' * self.__width)
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
 
     @property
     def width(self):
