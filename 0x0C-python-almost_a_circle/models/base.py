3f74bf1043265a591aa249e16196cc08c809541c
baragu <baraguantonyy@gmail.com>
2023-10-16 11:52:16 +0300

    chapter

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# base.py
+
+"""base class"""
+
+
+class Base:
+    """a base class
+private atributes:
+        __nb_objects
+class constructor:
+    def __init__(self, id=none)
+    """
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """the class constructor"""
+        if id:
+            self.id = id
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
