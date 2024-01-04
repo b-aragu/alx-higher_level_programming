4a18d07067cd1f47188524f8e69837c4c7ed37e0
baragu <baraguantonyy@gmail.com>
2024-01-05 00:58:20 +0300

    num of instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@
 class Rectangle:
     """define the self attributes"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
