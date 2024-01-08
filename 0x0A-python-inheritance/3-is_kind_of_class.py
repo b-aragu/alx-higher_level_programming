ddd2ec40bafadc4acbfe35549645b790c1245437
baragu <baraguantonyy@gmail.com>
2024-01-08 22:40:35 +0300

    hotsie

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" same class or inherits from """
+
+
+def is_kind_of_class(obj, a_class):
+    """ is an instance of returns true otherwise false """
+    return isinstance(obj, a_class)
