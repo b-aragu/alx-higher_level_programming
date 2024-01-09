f4a9059ff7a752248b293c77d8e3a3ea90a7cd71
baragu <baraguantonyy@gmail.com>
2024-01-09 21:07:13 +0300

    pascal

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,14 +9,14 @@ def pascal_triangle(n):
     returns a list of int representing the pascal triangle of n
     """
 
-    if n <=0:
+    if n <= 0:
         return []
-    
+
     triangle = [[1]]
     while len(triangle) != n:
         tri = triangle[-1]
         tmp = [1]
-        for i in range(len(tri) -1):
+        for i in range(len(tri) - 1):
             tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
         triangle.append(tmp)
