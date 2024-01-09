68f94d510caf207f6fffaee2016be4453fcb8e5f
baragu <baraguantonyy@gmail.com>
2024-01-09 17:53:59 +0300

    end=

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,4 @@ def read_file(filename=""):
     """ a function that reads a text file (UTF8) and prints it to stdout: """
 
     with open(filename, 'r', encoding="utf-8") as f:
-        data = f.read()
-        print(data)
+        print(f.read(), end="")
