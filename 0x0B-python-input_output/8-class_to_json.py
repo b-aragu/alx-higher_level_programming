e0da6c663ea927dc74128683523779f333ef2ba1
baragu <baraguantonyy@gmail.com>
2024-01-09 18:55:26 +0300

    .__dict__

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 8-class_to_json.py
+
+""" class to JSON """
+import json
+
+
+def class_to_json(obj):
+    """
+    Returns the dict description with simple data structure
+    args:
+        obj: an instance to a class
+    """
+    return obj.__dict__
