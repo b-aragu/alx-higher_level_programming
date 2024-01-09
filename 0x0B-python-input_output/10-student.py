d57bff177dea70d9c6074fe85b5050fce2703bc9
baragu <baraguantonyy@gmail.com>
2024-01-09 19:09:09 +0300

    attr

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 9-student.py
+
+""" student to JSON """
+
+
+class Student:
+    """
+    defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """ instation of class student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieve dict rep of Student instance"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {d: getattr(self, d) for d in attrs if hasattr(self, d)}
+        return self.__dict__
