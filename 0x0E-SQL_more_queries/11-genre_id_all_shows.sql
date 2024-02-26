e5043a0698cc07aa7d079eeec98cf5839361c9d2
baragu <baraguantonyy@gmail.com>
2024-02-26 07:20:30 +0300

    fix

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,5 @@
--- a script that lists all shows contained in the database hbtn_0d_tvshows.
--- Each record should display: tv_shows.title - tv_show_genres.genre_id
-SELECT `s`.`title`, `g`.`genre_id`
-	FROM `tv_shows` AS `s`
-	LEFT JOIN `tv_show_genres` AS `g`
-		ON `s`.`id` = `g`.`show_id`
-		ORDER BY `s`.`title`, `g`.`genre_id` ASC
+-- lists all shows contained in the database
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id;
