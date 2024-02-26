dde13f2253c1bc237c2c632a0509ad3142a4cfbd
baragu <baraguantonyy@gmail.com>
2024-02-26 07:14:27 +0300

    happy

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,10 +1,7 @@
-SELECT
-    city,
-    AVG((value * 9/5) + 32) AS avg_temp_fahrenheit
-FROM
-    temperatures
-GROUP BY
-    city
-ORDER BY
-    avg_temp_fahrenheit DESC;
-
+-- import database dump temperatures.sql to hbtn_0c_0 database
+-- then lists average temp by city ordered by temp (desc)
+-- source temperatures.sql;
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
