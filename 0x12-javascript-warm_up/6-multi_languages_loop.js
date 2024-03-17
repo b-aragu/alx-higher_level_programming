76d9af350499569464c8259a85b3593150cdb762
baragu <baraguantonyy@gmail.com>
2024-03-17 22:17:28 +0300

    array and loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* 6-multi_languages_loop.js
+ * Prints 3 lines
+ */
+
+arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
