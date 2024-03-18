a935dcf2dd7ee8e857b56978d47a52a2245cd7f0
baragu <baraguantonyy@gmail.com>
2024-03-18 10:39:00 +0300

    error fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
-/*
- * Prints messaage depending on value of argument passed
- * Usage: ./3-value_argument.js <arg>
- * Prints:
- *   - If no argument passed: "No argument" 
- *   - prints value if argument present
- */
-
 if (process.argv !== undefined) {
   console.log(process.argv[2]);
-}else{
+} else {
   console.log('No argument');
 }
-
