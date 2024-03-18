f8598ad4ba31a953d9454f60d027b0263c598d3f
baragu <baraguantonyy@gmail.com>
2024-03-18 11:58:50 +0300

    fix: logic and syntax flow

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -7,14 +7,15 @@
  */
 
 function factorial (n) {
-  if (isNaN(n)) {
-    return 1;
-  }
-  else{
-  if (n <= 1) {
+  if (n === 1) {
     return 1;
   } else {
     return n * factorial(n - 1);
-  }}
+  }
 }
-console.log(factorial(process.argv[2]));
+if (process.argv.length <= 2) {
+  console.log(1);
+}
+else {
+  console.log(factorial(parseInt(process.argv[2]));
+}
