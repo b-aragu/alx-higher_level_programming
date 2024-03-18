29cd8b0b215ffa7006881bb30427eed1459b5638
baragu <baraguantonyy@gmail.com>
2024-03-18 10:51:42 +0300

    7-multi.js:

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ * 7-multi_c.js
+ * Prints 'X' times "C is fun"
+ * X is the first arguement passed to the script
+ */
+if (process.argv.length === 3) {
+  const X = parseInt(process.argv[2]);
+  for (let i = 0; i < X; i++) {
+    console.log('C is fun');
+  }}
+else {
+  console.log('Missing number of occurrences');
+}
