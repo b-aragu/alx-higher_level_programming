c67301c3a9c396ad95f928a8847f2c02bed03766
baragu <baraguantonyy@gmail.com>
2024-03-19 23:04:43 +0300

    create: constructor for rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+ * 0-rectangle
+ * Empty class Rectangle that defines a rectangle
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
