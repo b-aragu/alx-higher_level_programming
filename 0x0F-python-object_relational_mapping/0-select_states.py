e2a246527c7cca1c77dca0d4e60121e82fd996dd
baragu <baraguantonyy@gmail.com>
2024-03-22 22:37:57 +0300

    add all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+This script lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
+
+    cursor = db_connect.cursor()
+
+    cursor.execute("SELECT * FROM states")
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
