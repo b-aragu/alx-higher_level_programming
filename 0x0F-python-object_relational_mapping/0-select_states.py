457e6db249ac93edc21bef5a28ed47505576a05a
baragu <baraguantonyy@gmail.com>
2024-03-22 22:40:52 +0300

    fix: import error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-
 """
-This script lists all states from the database hbtn_0e_0_usa
+This script lists all states from the
+database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """
     Access to the database and get the states
+    from the database.
     """
     db_connect = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-    )
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
-    cursor = db_connect.cursor()
+    db_cursor = db_connect.cursor()
 
-    cursor.execute("SELECT * FROM states")
+    db_cursor.execute("SELECT * FROM states")
 
-    states = cursor.fetchall()
+    rows_selected = db_cursor.fetchall()
 
-    for state in states:
-        print(state)
+    for row in rows_selected:
+        print(row)
