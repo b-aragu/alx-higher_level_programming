65df5c1d3e04f0e6c01e9377ae9832597b59f472
baragu <baraguantonyy@gmail.com>
2024-03-23 11:03:50 +0300

    fix: checker

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-/*
- * 102-add_me_maybe.js
- * function than increments and calls a function
- */
-
-exports.addMeMaybe = function addMeMaybe (number, theFunction) {
-  number = number + 1;
-  theFunction(number);
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
 };
