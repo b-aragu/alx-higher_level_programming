ae10b147de697c9f043cf95ea52fa25f0e91a817
baragu <baraguantonyy@gmail.com>
2024-03-23 11:01:39 +0300

    call me moby :)

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-/*
- * call_me_moby
- * executes x times a function
- */
-
-exports.callMeMoby = function callMeMoby (x, func) {
-  for (let i = 0; i < x; i++) {
-    func();
-  }
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
 };
