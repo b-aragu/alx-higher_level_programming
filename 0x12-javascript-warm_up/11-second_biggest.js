5f760a14b21b399f5158c2d33ad6d61a40ac6afa
baragu <baraguantonyy@gmail.com>
2024-03-23 11:00:04 +0300

    update: 11-second biggest change

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
-/*
- * 11-second_biggest.js
- * Search for the second biggest number in a list of arguments
- * Usage: ./11-second_biggest.js <arguments>
- */
-if (process.argv.length <= 2 || process.argv.length === 1) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let max = 0;
-  let second = 0;
-  for (let i = 2; i < process.argv.length; i++) {
-    if (parseInt(process.argv[i]) > max) {
-      second = max;
-      max = process.argv[i];
-    } else if (process.argv[i] > second) {
-      second = process.argv[i];
-    }
-  }
-  console.log(second);
+  const args = process.argv
+    .map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
