f1ecb19616e7816c7b6f48bf0b83a798912bbf12
baragu <baraguantonyy@gmail.com>
2024-04-03 17:38:16 +0300

    fix: checker to pass

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL, and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# send a request and only show the status code
+curl -s -o /dev/null -I --w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL, and displays the body of the response.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+# curl a json file
+curl -s -H "content-type:application/json"  -d @"$2" -X POST "$1"
